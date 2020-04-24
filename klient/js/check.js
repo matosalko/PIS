3c077bd3b8d046c9b289b3abaf82d1730a139465
Peter Sakalík <sakalikp@gmail.com>
2020-04-24 14:25:47 +0200

    refaktoring

--- klient/js/check.js
+++ klient/js/check.js
@@ -2,20 +2,27 @@ let changed_packages;
 let packages = []
 let user;
 let insurance;
+const changed_insurance_id = localStorage.getItem('changed_insurance_id');
+const insurance_id = localStorage.getItem('insurance_id');
+const user_id = localStorage.getItem('user_id');
 
 //ziska konkretnu originalnu poistku
 async function get_insurance() {
-    let response = await fetch(`/api/check_insurance`);
+    let response = await fetch(`/api/insurance/${insurance_id}`);
     let json = await response.json();
     insurance = json.body;
+
+    console.log('Vybrata poistka');
     console.log(insurance);
 }
 
 //ziska konkretneho pouzivatela, koho sa poistka tyka
 async function get_user() {
-    const response = await fetch(`/api/user_insurance/${insurance.user_id}`);
+    const response = await fetch(`/api/user/${user_id}`);
     const json = await response.json();
     user = json.body;
+
+    console.log('Vybraty pouzivatel');
     console.log(user);
 }
 
@@ -23,29 +30,26 @@ async function load_user() {
     await get_insurance();
     await get_user();
     
-    document.getElementById("name").innerHTML = `Meno poistenca: ${user.name} ${user.surname}`;
-    document.getElementById("insurance").innerHTML = `Cislo poistky: ${insurance.insurance_number}`;
+    document.getElementById('name').innerHTML = `Meno poistenca: ${user.name} ${user.surname}`;
+    document.getElementById('insurance').innerHTML = `Cislo poistky: ${insurance.insurance_number}`;
 }
 
-
-
-
-
 async function get_changed_packages() {
-    console.log("vytiahnutie zmenenych balikov");
-    const response = await fetch('/api/all_changed_packages');
+    const response = await fetch(`/api/all_changed_packages/${changed_insurance_id}`);
     const json = await response.json();
     changed_packages = json.body;
+
+    console.log('Vsetky zmenene baliky');
     console.log(changed_packages);
 }
 
 async function get_packages() {
-
     for(item of changed_packages) {
         const response = await fetch(`/api/package/${item.package_id}`);
         const json = await response.json();
         packages.push(json.body);
     }
+    console.log('Vsetky baliky');
     console.log(packages);
 }
 
@@ -70,20 +74,18 @@ async function load_packages() {
     }
 }
 
-
-
 async function accept() {
     let message = '';
     let state = 'prijata';
-    let id = changed_packages[0].changed_insurance_id;
 
     const options = {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({id}) //data odosielane v requeste
+        body: JSON.stringify({changed_insurance_id})
     };
+
     await fetch('/api/remove_changed_packages', options);
 
     set_msg_state(message, state);
@@ -91,10 +93,10 @@ async function accept() {
     notify(user, message, state);
 }
 
-async function change() {
+function change() {
     document.location.href = '/html/edit_changes.html'
 }
 
-async function denie() {
+function denie() {
     document.location.href = '/html/denie.html'
 }
--- klient/js/employee.js
+++ klient/js/employee.js
@@ -8,6 +8,7 @@ async function get_changed_insurances() {
     const response = await fetch('/api/all_changed_insurances');
     const json = await response.json();
     changed_insurances = json.body;
+
     console.log('Zmenene poistky');
     console.log(changed_insurances);
 }
@@ -24,31 +25,33 @@ async function get_insurances() {
     console.log(insurances);
 }
 
-async function myFunction(x) {
-    let ids = x.id.split(" ");
+function set_ids(ids) {
+    ids = ids.split(' ');
 
-    await fetch(`/api/set_insurance/${ids[1]}`);
-    await fetch(`/api/set_changed_insurance/${ids[0]}`);
-    changed_insurance_id = ids[0];
+    localStorage.setItem('changed_insurance_id', ids[0]);
+    localStorage.setItem('insurance_id', ids[1]);
+
+    console.log('ID poistky a jej zmenenej verzie');
+    console.log(ids[1] + ' ' + ids[1]);
 }
 
 async function create_table() {
     await get_changed_insurances();
     await get_insurances();
 
-    for(item of insurances) {
-        let tbl = document.getElementById("ins_table");
-        let tr = document.createElement("tr");
-        let td1 = document.createElement("td");
-        let td2 = document.createElement("td");
+    let counter = 1;
 
-        let text1 = `${item.change_ins_id}`;
+    for(item of insurances) {
+        let tbl = document.getElementById('ins_table');
+        let tr = document.createElement('tr');
+        let td1 = document.createElement('td');
+        let td2 = document.createElement('td');
+
+        let text1 = `${counter}`;
         let text2 = `${item.insurance_number}`;
-        
 
         td2.setAttribute('id', `${item.change_ins_id} ${item.id}`);
-        td2.setAttribute('onclick', "myFunction(this)")
-
+        td2.setAttribute('onclick', 'set_ids(this.id)');
 
         td1.appendChild(document.createTextNode(text1));
         td2.appendChild(document.createTextNode(text2));
@@ -56,19 +59,11 @@ async function create_table() {
         tr.appendChild(td1);
         tr.appendChild(td2);
         tbl.appendChild(tr);
+
+        counter += 1;
     }
 }
 
-async function change_scene() {
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({changed_insurance_id}) //data odosielane v requeste
-    };
-
-    await fetch('/api/set_changed_insurance/', options);
-    console.log("button");
+function change_scene() {
     document.location.href = '/html/check.html';
 }
